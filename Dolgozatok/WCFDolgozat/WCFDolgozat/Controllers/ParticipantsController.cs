83b2485108f9f43b68eab60a027c49328c62015f
haluskad <haluskad@201-14>
2025-09-24 09:27:58 +0200

    WCFDolgozat , 09.24 ,

---
+++ Dolgozatok/WCFDolgozat/WCFDolgozat/Controllers/ParticipantsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WCFDolgozat.Controllers
+{
+    public class ParticipantsController
+    {
+        string[] adatok = File.ReadAllLines("C:\\Users\\haluskad\\Downloads\\Participants.txt").Skip(1).ToArray();
+        List<Participants> participants = new List<Participants>();
+
+        public List<Participants> GetParticipants()
+        { 
+            foreach (var item in adatok)
+            {
+                var sor = item.Split(';');
+                participants.Add(new Participants
+                {
+                    Id = int.Parse(sor[0]),
+                    Name = sor[1],
+                    Age = int.Parse(sor[2]),
+                    AverageScore = double.Parse(sor[3])
+                });
+            }
+            return participants;
+
+
+        }
+
+       
+
+
+    }
+}
