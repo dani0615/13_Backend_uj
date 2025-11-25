93df3b9b44fbfb421fc18a3161aaf947a3115b86
Dani Haluska <haluskadaniel06@gmail.com>
2025-11-25 15:24:18 +0100

    Xaml megjelenítési hibák fixelve , adatok megjelennek , menü javítva

--- Orai_feladatok/HashGenerator/HashGenerator/MainWindow.xaml.cs
+++ Orai_feladatok/HashGenerator/HashGenerator/MainWindow.xaml.cs
@@ -71,6 +71,7 @@ namespace HashGenerator
             StreamWriter sw = new StreamWriter("hash.txt");
             sw.WriteLine(tbxLogin.Text);
             sw.WriteLine(tbxPassword.Text);
+            sw.WriteLine(tbxSalt.Text);
             sw.WriteLine(tbxHash.Text);
             sw.WriteLine(tbxDoubleHash.Text);
             sw.Close();
@@ -85,7 +86,7 @@ namespace HashGenerator
 
         private void Menu_About_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hash Generator Pro v1.0\n\nKészítette: Dragon\nBiztonságos jelszóhash generátor SHA-256 algoritmussal.",
+            MessageBox.Show("Hash Generator Pro v1.0\n\nBiztonságos jelszóhash generátor SHA-256 algoritmussal.",
                             "Névjegy", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
